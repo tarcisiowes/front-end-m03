fd74379a47027caeb1b4f39c8ebd04a70fa9597e
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-06-07 20:38:48 -0300

    a06-front-pokeapi-navbar+style

---
+++ a06-front-design-system-and-libs/classe-01/src/components/navbar/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import useStyles from './styles'
+
+
+
+
+export default function NavBar() {
+
+  const classes = useStyles();
+
+  return (
+
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+      >
+        <Toolbar className={classes.toolbar}>
+          <Typography variant="h6" noWrap className={classes.title}>
+            Pokedex
+          </Typography>
+          
+        </Toolbar>
+      </AppBar>
+      
+    </div>
+  );
+}
---
+++ a06-front-design-system-and-libs/classe-01/src/components/navbar/styles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/styles'
+
+export default makeStyles((theme) => ({
+    
+  root: {
+    display: 'flex',
+  },
+  title: {
+    flexGrow: 1,
+  },
+    toolbar: {
+      backgroundColor: 'darkred'
+  }
+  
+}));
